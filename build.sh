f9853a8ac0949cda05e600ce8ccd1e87db8bcdfd
nim-ka <raumaank1@gmail.com>
2022-07-23 15:36:16 +0300

    init

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat todo.txt \
+	proto_phono.txt \
+	proto_ortho.txt \
+	changes_from_proto.txt \
+	phono.txt \
+	phonotactics.txt \
+	ortho.txt \
+	vowel_harmony.txt \
+	morpho.txt \
+	given_words.txt > clong.txt
